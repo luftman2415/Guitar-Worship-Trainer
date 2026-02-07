b6b300cbc59ee607f2a876f51bac32f24d4f45b4
luftman2415 <corazonesjuntos28@gmail.com>
2026-02-07 14:31:40 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'gwt-v1';
+const CACHE_NAME = 'gwt-v2'; // Cambiamos a v2 para limpiar errores antiguos
 const ASSETS = [
   './',
   './index.html',
@@ -12,9 +12,24 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
