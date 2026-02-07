c03dc5fbcfbf9bf51db339f565accba1c103dd8c
luftman2415 <corazonesjuntos28@gmail.com>
2026-02-07 14:39:49 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,43 +1,20 @@
 
-const CACHE_NAME = 'gwt-v2'; // Cambiamos a v2 para limpiar errores antiguos
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  'https://cdn.tailwindcss.com',
-  'https://unpkg.com/@babel/standalone/babel.min.js',
-  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap'
-];
-
+// Service Worker vacío para limpiar la caché del navegador
 self.addEventListener('install', (event) => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames.map((cacheName) => caches.delete(cacheName))
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  // No cachear nada, simplemente dejar pasar la petición
+  return fetch(event.request);
 });
