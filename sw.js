1fba116df343ebe7b3c2d5f95964080bee6818a6
luftman2415 <corazonesjuntos28@gmail.com>
2026-05-14 15:26:17 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-
-const CACHE_NAME = 'gwt-pro-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))));
-self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request))));
